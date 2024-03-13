f00e53c736cfd1acdff21c33be7cb6224537954b
Henry Asa <henryjacobasa@gmail.com>
2024-03-12 22:04:49 -0400

    More progress and updates

--- collecting_data.py
+++ collecting_data.py
@@ -1,15 +1,10 @@
-import math
 import time
 import numpy as np
 import wavio
 import matplotlib.pyplot as plt
 import os
-import wave
 import sounddevice as sd
-from pydub import AudioSegment
-from pydub.playback import play
 from pathlib import Path
-import threading
 import queue
 import soundfile as sf
 import sys
@@ -50,40 +45,7 @@ def generate_audio_files():
                 print(f'sine_F{frequency}_S{sample_rate}_B{bitrate}.wav')
 
 
-# q = queue.Queue()
-
-# def callback(indata, frames, time, status):
-#     """This is called for each audio block from the microphone"""
-#     if status:
-#         print(status)
-#     q.put(indata.copy())
-
-# def record_audio(duration, samplerate):
-#     """Record audio from the microphone"""
-#     with sd.InputStream(samplerate=samplerate, channels=1, callback=callback, dtype='float32'):
-#         sd.sleep(int(duration * 1000))
-
-# def play_audio(filename):
-#     """Play an audio file"""
-#     # Open the WAV file
-#     wf = wave.open(filename, 'rb')
-#     # Get audio file properties
-#     samplerate = wf.getframerate()
-#     channels = wf.getnchannels()
-#     # Start playback
-#     stream = sd.OutputStream(samplerate=samplerate, channels=channels)
-#     stream.start()
-#     data = wf.readframes(1024)
-#     while len(data) > 0:
-#         stream.write(data)
-#         data = wf.readframes(1024)
-#     stream.stop()
-#     stream.close()
-#     wf.close()
-
-
 q = queue.Queue()
-
 
 def callback_in(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
@@ -114,134 +76,24 @@ def record_audio_samples():
         output_filename = f'result_{filetype}_F{freq}_S{sample_rate}_B{bit_depth}.wav'
         os.makedirs(f'{output_folder}/F{freq}', exist_ok=True)
         print(filename)
-        song: AudioSegment = AudioSegment.from_wav(filename)
-        # print(f'{song.sample_width}  {song.frame_rate}  {song.frame_count()}  {song.frame_width}')
-        # song = AudioSegment.from_wav("audio_file.wav")
-        # you can also read from other formats such as MP4
-        # song = AudioSegment.from_file("audio_file.mp4", "mp4")
-        # play(song)
         
 
-        # Extract the samplerate from the audio file
         filename = str(filename)
-        # filename = "04 - David Elias - The Window - Vision of Her (16b-44.1).wav"
-        # filename = "short_song.wav"
-        # output_filename = "testA.wav"
-        # wf = wave.open(filename, 'rb')
-        # samplerate = wf.getframerate()
-        # wf.close()
-
-        # # Start recording and playback threads
-        # record_thread = threading.Thread(target=record_audio, args=(6, 192000))
-        # play_thread = threading.Thread(target=play_audio, args=(filename,))
-
-        # record_thread.start()
-        # play_thread.start()
-
-        # # Wait for playback to finish
-        # play_thread.join()
-        # # Wait for recording to finish
-        # record_thread.join()
-
-        # # Save the recorded audio to a WAV file
-        # with wave.open(f'{output_folder}/F{freq}/{output_filename}', 'wb') as wave_file:
-        #     wave_file.setnchannels(1)
-        #     wave_file.setsampwidth(3)
-        #     wave_file.setframerate(192000)
-        #     while not q.empty():
-        #         frame = q.get()
-        #         wave_file.writeframes(frame)
         sound_data, _fs = sf.read(filename)
-        print(len(sound_data) / _fs)
-        print(_fs)
-
-        # recorded_audio = sd.playrec(
-        #     data=sound_data,
-        #     samplerate=_fs,
-        #     # data=np.array(song.get_array_of_samples()).astype(np.float64),
-        #     # samplerate=int(sample_rate),
-        #     channels=1,
-        #     blocking=True,
-        # )
-        
-
-
-
-        # # recorded_audio = np.empty(shape=math.ceil(RECORDING_SAMPLE_RATE * (len(sound_data)/_fs)))
-        # print("ABOUT TO START RECORDING")
-        # # recorded_audio = sd.rec(out=recorded_audio, samplerate=RECORDING_SAMPLE_RATE, channels=1, dtype="float64")
-        # recording_frames = math.ceil(RECORDING_SAMPLE_RATE * (len(sound_data)/_fs))
-        # recorded_audio = sd.rec(frames=recording_frames, samplerate=RECORDING_SAMPLE_RATE, channels=1, dtype="float64")
-        # print("ABOUT TO START PLAYING")
-        # # sd.play(
-        # #     data = sound_data,
-        # #     samplerate=_fs,
-        # #     blocking=True,
-        # # )
-        # print("FINISHED PLAYING")
-        # print(recorded_audio)
-
-        # # audio = AudioSegment(recorded_audio)
-        # wavio.write(f'{output_folder}/F{freq}/{output_filename}', recorded_audio, RECORDING_SAMPLE_RATE, sampwidth=4)
-
-
-
-
-        # # print(audio.get_array_of_samples())
-
-        # # with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
-        # #               channels=args.channels, subtype=args.subtype) as file:
-        # recording = sd.InputStream(samplerate=RECORDING_SAMPLE_RATE,
-        #                     channels=1, callback=callback_in)
-        #     # print('#' * 80)
-        #     # print('press Ctrl+C to stop the recording')
-        #     # print('#' * 80)
-        # print("ABOUT TO START PLAYING")
-        # sd.play(
-        #     data = sound_data,
-        #     samplerate=_fs,
-        #     blocking=True,
-        # )
-        # # recorded_audio = q.get()
-        # sd.stop()
-        # print(recording.read(recording.read_available))
-        # recording.abort()
-        # wavio.write(f'{output_folder}/F{freq}/{output_filename}', recording, RECORDING_SAMPLE_RATE, sampwidth=4)
-
-
-
-        # print(audio.get_array_of_samples())
         duration = len(sound_data)/_fs
+        print(f'\tF = {freq} Hz\tS = {sample_rate}\tB = {bit_depth}\tDURATION = {duration}')
+
 
         with sf.SoundFile(f'{output_folder}/F{freq}/{output_filename}', mode='w+', samplerate=RECORDING_SAMPLE_RATE,
                       channels=1, subtype='PCM_32', format='WAV') as file:
             with sd.InputStream(samplerate=RECORDING_SAMPLE_RATE, channels=1, callback=callback_in):
-                # print('#' * 80)
-                # print('press Ctrl+C to stop the recording')
-                # print('#' * 80)
-                print("ABOUT TO START PLAYING")
                 sd.play(data = sound_data, samplerate=_fs, blocking=False)
                 start_time = time.time()
                 while time.time() - start_time <= duration:
                     file.write(q.get())
-        
 
-                
-        # recorded_audio = q.get()
         sd.stop()
-        raise TypeError
 
-
-    raise TypeError
-    fs=19200
-    duration = 10  # seconds
-    myrecording = sd.rec(duration * fs, samplerate=fs, channels=2, dtype='float64')
-    print("Recording Audio for %s seconds" %(duration))
-    sd.wait()
-    print("Audio recording complete , Playing recorded Audio")
-    sd.play(myrecording, fs)
-    sd.wait()
-    print("Play Audio Complete")
 
 if __name__ == "__main__":
     record_audio_samples()
