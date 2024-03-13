95268a7a2302c9be637a3001d9ff879dd47611be
Henry Asa <henryjacobasa@gmail.com>
2024-03-12 23:01:47 -0400

    Cleaned up data collection script

--- collecting_data.py
+++ collecting_data.py
@@ -1,6 +1,5 @@
+from datetime import datetime
 import time
-import numpy as np
-import wavio
 import matplotlib.pyplot as plt
 import os
 import sounddevice as sd
@@ -9,41 +8,12 @@ import queue
 import soundfile as sf
 import sys
 
-
-
 from signals import Signal, Sine
-
-folder_name = "generated_audio"
-
-
-frequency = 440.0                       # sound frequency (Hz)
-sample_duration = 5                     # sample duration (seconds)
-
-def generate_single_frequency(frequency, sample_duration, sample_rate, sample_bit_rate, folder_name):
-    sample_number = int(sample_rate * sample_duration)          # number of samples
-    time_vals = np.arange(sample_number)/sample_rate            # grid of time values
-    x = np.sin(2*np.pi * frequency * time_vals)
-    wavio.write(f'{folder_name}/sine_F{frequency}_S{sample_rate}_B{sample_bit_rate}.wav', x, sample_rate, sampwidth=int(sample_bit_rate/8))
+from generate_audio import generate_audio_files
 
 
-
-def generate_audio_files():
-    frequencies = [440, 1000, 2000, 4000, 8000, 12000, 16000]
-    sample_rates = [1000, 2000, 4000, 8000, 16000, 44100, 82000, 96000, 192000]
-    bitrates = [8, 16, 24]
-    sample_duration = 5
-
-    for freq in frequencies:
-        folder_name = f'F{freq}'
-        for sample_rate in sample_rates:
-            for bitrate in bitrates:
-                if sample_rate <= 2*freq:
-                    ## ALIASING IS OCCURING
-                    continue
-                os.makedirs(f'audio_test_samples/{folder_name}', exist_ok=True)
-                generate_single_frequency(freq, sample_duration, sample_rate, bitrate, f'audio_test_samples/{folder_name}')
-                print(f'sine_F{frequency}_S{sample_rate}_B{bitrate}.wav')
-
+RECORDING_SAMPLE_RATE = 96000
+"""Default sampling rate of 96 kHz for recorded samples"""
 
 q = queue.Queue()
 
@@ -54,16 +24,20 @@ def callback_in(indata, frames, time, status):
     q.put(indata.copy())
 
 
-def record_audio_samples():
+def record_audio_samples(
+        audio_samples_directory: str = "audio_test_samples",
+        output_directory: str = "generated_audio"
+    ):
 
-    RECORDING_SAMPLE_RATE = 96000
+    print(sd.query_devices())
+    sd.default.device = 3, 2
+    # raise TypeError
 
     print(f'INPUT SETTINGS: \t{sd.check_input_settings()}')
     print(f'OUTPUT SETTINGS:\t{sd.check_output_settings()}')
 
-    audio_files = list(Path("audio_test_samples").rglob("*.wav"))
-    output_folder = f'recorded_samples'
-    os.makedirs(output_folder, exist_ok=True)
+    audio_files = list(Path(audio_samples_directory).rglob("*.wav"))
+    os.makedirs(output_directory, exist_ok=True)
 
     for filename in audio_files:
         # read MP3 file
@@ -74,20 +48,18 @@ def record_audio_samples():
         bit_depth = file_params[-1].split(".")[0][1:]
         
         output_filename = f'result_{filetype}_F{freq}_S{sample_rate}_B{bit_depth}.wav'
-        os.makedirs(f'{output_folder}/F{freq}', exist_ok=True)
+        os.makedirs(f'{output_directory}/F{freq}', exist_ok=True)
         print(filename)
-        
 
         filename = str(filename)
-        sound_data, _fs = sf.read(filename)
-        duration = len(sound_data)/_fs
+        audio_data, sample_rate = sf.read(filename)
+        duration = len(audio_data)/sample_rate
         print(f'\tF = {freq} Hz\tS = {sample_rate}\tB = {bit_depth}\tDURATION = {duration}')
 
-
-        with sf.SoundFile(f'{output_folder}/F{freq}/{output_filename}', mode='w+', samplerate=RECORDING_SAMPLE_RATE,
+        with sf.SoundFile(f'{output_directory}/F{freq}/{output_filename}', mode='w+', samplerate=RECORDING_SAMPLE_RATE,
                       channels=1, subtype='PCM_32', format='WAV') as file:
             with sd.InputStream(samplerate=RECORDING_SAMPLE_RATE, channels=1, callback=callback_in):
-                sd.play(data = sound_data, samplerate=_fs, blocking=False)
+                sd.play(data = audio_data, samplerate=sample_rate, blocking=False)
                 start_time = time.time()
                 while time.time() - start_time <= duration:
                     file.write(q.get())
@@ -96,26 +68,8 @@ def record_audio_samples():
 
 
 if __name__ == "__main__":
-    record_audio_samples()
-    generate_audio_files()
-    # frequency = 440
-    # sample_duration = 5
+    initial_time = datetime.now().strftime("%m-%d_%H-%M")
+    shared_output_directory = f'data/{initial_time}'
 
-    # test_signal_1000_8 = Sine(frequency=frequency, time_length=sample_duration, sample_rate=1000, bitrate=8)
-    # test_signal_44100_8 = Sine(frequency=frequency, time_length=sample_duration, sample_rate=44100, bitrate=8)
-    # test_signal_44100_16 = Sine(frequency=frequency, time_length=sample_duration, sample_rate=44100, bitrate=16)
-
-    # test_signal_1000_8_audio = Sine.from_wav(f'{folder_name}/sine_F{frequency}_S1000_B8.wav', bitrate=8, frequency=frequency)
-    # test_signal_1000_8_discrete = Sine(frequency=frequency, time_length=sample_duration, sample_rate=1000, bitrate=8, discretize = True)
-
-
-    # num_waves_to_plot = 5
-
-    # test_signal_1000_8.plot_waveform(num_waves=num_waves_to_plot)
-    # test_signal_1000_8_audio.plot_waveform(num_waves=num_waves_to_plot)
-    # test_signal_1000_8_discrete.plot_waveform(num_waves=num_waves_to_plot)
-    # # test_signal_44100_8.plot_waveform(num_waves=num_waves_to_plot)
-    # # test_signal_44100_16.plot_waveform(num_waves=num_waves_to_plot)
-
-    # plt.show()
-    raise TypeError
+    generate_audio_files(output_directory=f'{shared_output_directory}/audio_test_samples')
+    record_audio_samples(output_directory=f'{shared_output_directory}/recorded_samples')
