5612040df9a70758ece66122ed7c6c0edb46162c
Henry Asa <henryjacobasa@gmail.com>
2024-03-14 01:17:30 -0400

    Added a few more constants to `constants`

--- constants.py
+++ constants.py
@@ -1,4 +1,12 @@
+"""`constants`
 
+Contains common constant value definitions that are used throughout
+the code repository.  Serves as the "source of truth" for common
+constant values
+"""
+
+DATA_DIRECTORY = "data"
+"""Default directory to store all of the raw data files"""
 
 MP3_BITRATES = [
         "32k",  # Generally acceptable only for speech
@@ -11,3 +19,5 @@ MP3_BITRATES = [
     ]
 """Typical Bitrates for MP3 files, in the readable type for pydub"""
 
+RECORDING_SAMPLE_RATE = 96000
+"""Default sampling rate of 96 kHz for recorded samples"""
