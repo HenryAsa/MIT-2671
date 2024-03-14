b1a9072259fe6b6e1441b615a91511ae1d1e7749
Henry Asa <henryjacobasa@gmail.com>
2024-03-14 00:47:09 -0400

    Defined a `constants` file so that all constant values live in one place

---
+++ constants.py
@@ -0,0 +1,13 @@
+
+
+MP3_BITRATES = [
+        "32k",  # Generally acceptable only for speech
+        "96k",  # Generally used for speech or low-quality streaming.
+        "128k", # mid-range bitrate quality.
+        "160k", # mid-range bitrate quality.
+        "192k", # medium quality bitrate.
+        "256k", # a commonly used high-quality bitrate.
+        "320k", # highest bitrate supported by MP3 standard
+    ]
+"""Typical Bitrates for MP3 files, in the readable type for pydub"""
+
