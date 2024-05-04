bfee3d374e57f0a7d8ddc3a50e5b81871b27b8d3
Henry Asa <henryjacobasa@gmail.com>
2024-05-04 19:21:55 -0400

    Streamlined sound generation file

--- testing_sound_generation.py
+++ testing_sound_generation.py
@@ -1,11 +1,17 @@
-from audio_frequency_generation import generate_single_frequency
+from audio_frequency_generation import generate_single_frequency_audio_files
+from constants import DATA_DIRECTORY
+from datetime import datetime
 
 
 
 if __name__ == "__main__":
+    initial_time = datetime.now().strftime("%m-%d_%H-%M")
+    output_directory = f'{DATA_DIRECTORY}/frequencies/{initial_time}'
+
     frequency = 440
     sample_duration = 5
 
-    for sample_rate in [1000, 44100, 88200, 96000, 192000]:
-        for bit_depth in [8, 16, 24]:
-            generate_single_frequency(frequency, sample_rate, bit_depth, sample_duration)
+    generate_single_frequency_audio_files(
+        sample_rates=[1000, 2000, 4000, 8000, 44100, 88200, 96000, 192000],
+        output_directory=output_directory,
+    )
